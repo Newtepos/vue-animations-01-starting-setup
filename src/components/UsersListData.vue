35665ca4038acce19729ed9e4eeed697f14bd233
Supakit <supakit.pnm@gmail.com>
2022-06-17 16:33:35 +0700

    Add Transition Group

--- src/components/UsersListData.vue
+++ src/components/UsersListData.vue
@@ -1,7 +1,9 @@
 <template>
-  <ul>
-    <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-  </ul>
+  <transition-group tag="ul" name="user-list">
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </transition-group>
   <div>
     <input type="text" ref="userNameInput" />
     <button @click="addUser">Add User</button>
@@ -20,9 +22,9 @@ export default {
       this.users.unshift(this.$refs.userNameInput.value);
     },
     removeUser(user) {
-      this.users = this.users.filter(usr => usr != user);
-    }
-  }
+      this.users = this.users.filter((usr) => usr != user);
+    },
+  },
 };
 </script>
 
@@ -37,4 +39,38 @@ li {
   padding: 1rem;
   text-align: center;
 }
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+
+.user-list-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.user-list-leave-active {
+  transition: all 1s ease-in;
+  position: absolute;
+}
+
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.user-list-move {
+  transition: transform 0.8s ease;
+}
+
 </style>
