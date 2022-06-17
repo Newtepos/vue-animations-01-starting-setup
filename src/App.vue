4e7b66866b1f3249c2252e94f6aa10302b139f52
Supakit <supakit.pnm@gmail.com>
2022-06-17 15:31:20 +0700

    Add: transition Component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class="{ animate: amimatedBlock }"></div>
     <button @click="AnimateBlock">Animate</button>
   </div>
+  <div class="container">
+    <transition>
+      <p v-if="pasaIsVisible">This is only sometime visible</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -15,7 +21,11 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false, amimatedBlock: false };
+    return {
+      dialogIsVisible: false,
+      amimatedBlock: false,
+      pasaIsVisible: false,
+    };
   },
   methods: {
     showDialog() {
@@ -26,6 +36,9 @@ export default {
     },
     AnimateBlock() {
       this.amimatedBlock = true;
+    },
+    toggleParagraph() {
+      this.pasaIsVisible = !this.pasaIsVisible;
     },
   },
 };
@@ -77,6 +90,34 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-fade 0.3s ease-out forwards;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 
 @keyframes slide-fade {
